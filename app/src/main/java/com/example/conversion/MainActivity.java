461fe66edf037fa6884326779708994758c65bfa
papa matar fall <matar026@gmail.com>
2022-10-22 13:30:06 +0000

    commte

---
+++ app/src/main/java/com/example/conversion/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.conversion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView =findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String montant="14.1";
+                Float energie = Float.parseFloat(montant)/141;
+                String senergie=""+energie;
+                textView.setText(senergie);
+
+            }
+        });
+
+    }
+}
